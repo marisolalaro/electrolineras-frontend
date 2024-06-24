eebc46da4de8495430f689eaca403614b3120472
Viviana Machaca <viviana.machaca@et.bo>
2024-06-24 17:05:32 -0400

    creacion de la factura pdf

--- src/app/modules/invoice-electric-stations/invoice-electric-stations.component.ts
+++ src/app/modules/invoice-electric-stations/invoice-electric-stations.component.ts
@@ -123,11 +123,10 @@ export default class InvoiceElectricStationsComponent {
   onOpenFactura(item) {
     const xmlContext = this.base64aXML.decodeBase64(item.xmlBase64);
     const jsonData: any = xmlToJsonUtil(xmlContext);
+    console.log(JSON.stringify(jsonData) );
+    
     var doc = this.invoiceTransaction.getFactura(jsonData);
     pdfMake.createPdf(doc).open();
-    // this.base64ToPdfService.convertBase64ToPdf(item.xmlBase64, outputFileName);
-    // this.pdfService.generatePdfInvoiceTransaction(xmlContext);
-
   }
 
   onOpenImagenQR(item) {
