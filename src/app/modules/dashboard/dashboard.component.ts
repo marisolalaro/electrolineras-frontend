15623154a8eae966553c3df5aa0e4fa239458c7f
Viviana Machaca <viviana.machaca@et.bo>
2024-09-10 14:25:13 -0400

    version3 reconexion

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -127,7 +127,8 @@ export default class DashboardComponent implements OnInit, OnDestroy {
       if (this.websocketService.isConnected()) { // Supongamos que tienes una función para verificar el estado
         this.cambiarEstadoHeartbeat('En línea');
       } else {
-        this.conectaWebSocket()
+        this.conectaWebSocket();
+        this.escucharHeartbeat()
       }
     });
     this.subscriptions.push(this.reconnectionCheckSubscription);
