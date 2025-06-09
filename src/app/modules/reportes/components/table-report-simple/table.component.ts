6412bca918ed6a600584322f017508171b65a342
viviana.machaca <viviana.machaca@et.bo>
2025-06-09 14:47:50 -0400

    Se adiciono nro de facturas a los reportes

--- src/app/modules/reportes/components/table-report-simple/table.component.ts
+++ src/app/modules/reportes/components/table-report-simple/table.component.ts
@@ -91,6 +91,7 @@ export class TableReportSimpleComponent {
     return new Promise((resolve) => {
       if (this.reporteX == 1) {
         this.cols = [
+          { field: 'id', header: 'Factura Número', pipe: '' },
           { field: 'razonSocial', header: 'Nombre/Razón Social', pipe: '' },
           { field: 'nitCi', header: 'Nit/Ci', pipe: '' },
           { field: 'cuf', header: 'Cuf', pipe: '' },
@@ -103,6 +104,7 @@ export class TableReportSimpleComponent {
       }
       if (this.reporteX == 2) {
         this.cols = [
+          { field: 'id', header: 'Factura Número' },
           { field: 'razonSocial', header: 'Nombre/Razón Social' },
           { field: 'numeroDocumento', header: 'Número de Documento' },
           { field: 'bank', header: 'Banco' },
@@ -115,6 +117,7 @@ export class TableReportSimpleComponent {
       }
       if (this.reporteX == 4) {
         this.cols = [
+          { field: 'id', header: 'Factura Número'},
           { field: 'codigoDescripcion', header: 'Código' },
           { field: 'fechaRegistro', header: 'Fecha de Registro' },
           { field: 'codigoRecepcion', header: 'Recepción' },
