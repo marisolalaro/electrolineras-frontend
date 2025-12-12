b606351aa0f86ff1aedb6c3732e3caf585ed6a40
viqui.alaro <viqui.alaro@endesyc.bo>
2025-12-12 11:15:11 -0400

    environment

--- src/app/core/environments/environment.prod.ts
+++ src/app/core/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: true,
     //  apiUrl: 'http://localhost:8050/electrolinerasbackend', // server localhost
     //  apiUrl: 'http://10.241.74.31:8050/electrolinerasbackend', // server ende ip
-     apiUrl: 'https://test-dlpelectrolineras.et.bo/electrolinerasbackend', // server test
-    //  apiUrl: 'https://dlpelectrolineras.et.bo/electrolinerasbackend', // server prod
+    //  apiUrl: 'https://test-dlpelectrolineras.et.bo/electrolinerasbackend', // server test
+      apiUrl: 'https://dlpelectrolineras.et.bo/electrolinerasbackend', // server prod
 
     // apiUrlOcpp: 'http://localhost:8051', // server localhost
     // apiUrlOcpp: 'http://10.241.74.31:8051', // server ende ip
